248e4c47b3af97f90b0abdc8549f1981f7d3ebf2
mikecode <102126994+miguelbtcode@users.noreply.github.com>
2025-02-16 21:41:27 -0500

    Update vite.config.js

--- frontend/club-key-app/vite.config.js
+++ frontend/club-key-app/vite.config.js
@@ -3,15 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5084',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  base: '/',
+  build: {
+    outDir: 'dist'
+  }
 })
