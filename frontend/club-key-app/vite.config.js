1fee71015e2730647eb54f05edc34d99c6963c34
mikecode <mabt2206@gmail.com>
2025-02-16 22:18:05 -0500

    Update vite config js x2

--- frontend/club-key-app/vite.config.js
+++ frontend/club-key-app/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
